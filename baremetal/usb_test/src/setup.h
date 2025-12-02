e99df4da1ea73b7984ccbe3423e0fa6f183758d1
Jakob Kastelic <jkastelic@thinksrs.com>
2025-12-02 11:10:56 -0800

    add usb_test

---
+++ baremetal/usb_test/src/setup.h
@@ -0,0 +1,42 @@
+#ifndef SETUP_H
+#define SETUP_H
+
+#include "stm32mp13xx_hal.h"
+#include "stm32mp13xx_hal_pcd.h"
+#include <string.h>
+
+// global variables
+extern SD_HandleTypeDef SDHandle;
+extern PCD_HandleTypeDef hpcd_USB_OTG_HS;
+
+// clocks and memory
+void SystemClock_Config(void);
+void PeriphCommonClock_Config(void);
+void setup_ddr(void);
+
+// HAL MSP
+int HAL_DDR_MspInit(ddr_type type);
+void HAL_UART_MspInit(UART_HandleTypeDef* huart);
+void HAL_UART_MspDeInit(UART_HandleTypeDef* huart);
+void HAL_SD_MspInit(SD_HandleTypeDef *hsd);
+
+// pin mux
+void MX_UART4_Init(void);
+
+// debugging
+int __io_putchar(int ch);
+int __io_getchar (void);
+void Error_Handler(void);
+
+// SD
+SD_HandleTypeDef setup_sd(void);
+
+// USB
+void MX_USB_OTG_HS_PCD_Init(void);
+void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle);
+void HAL_PCD_MspDeInit(PCD_HandleTypeDef* pcdHandle);
+
+#endif // SETUP_H
+
+// end file setup.h
+
