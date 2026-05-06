d3c600aa69220ec4f55adeab9da8f57148dc1b64
Jakob Kastelic <kastelic.jakob@gmail.com>
2026-05-06 09:53:44 -0700

    gpio_test: add IO1 low UART trigger 'r' command

--- baremetal/gpio_test/src/main.c
+++ baremetal/gpio_test/src/main.c
@@ -13,6 +13,7 @@ int gpio_connectivity_mpu_replay_stub_run(void);
 int gpio_connectivity_mpu_replay_io0_high_report(void);
 int gpio_connectivity_mpu_replay_io0_low_report(void);
 int gpio_connectivity_mpu_replay_io1_high_report(void);
+int gpio_connectivity_mpu_replay_io1_low_report(void);
 int gpio_connectivity_mpu_replay_io2_high_report(void);
 int gpio_connectivity_mpu_replay_io3_high_report(void);
 int gpio_connectivity_mpu_replay_io0_sample_report(void);
@@ -29,6 +30,7 @@ static volatile int replay_status;
 static volatile int io0_hold_high;
 static volatile int io0_hold_low;
 static volatile int io1_hold_high;
+static volatile int io1_hold_low;
 static volatile int io2_hold_high;
 static volatile int io3_hold_high;
 static volatile int ncs_hold_low;
@@ -48,6 +50,9 @@ static void gpio_test_handle_command(char command)
    } else if (command == '1') {
       if (gpio_connectivity_mpu_replay_io1_high_report())
          io1_hold_high = 1;
+   } else if (command == 'r') {
+      if (gpio_connectivity_mpu_replay_io1_low_report())
+         io1_hold_low = 1;
    } else if (command == '2') {
       if (gpio_connectivity_mpu_replay_io2_high_report())
          io2_hold_high = 1;
@@ -89,7 +94,8 @@ int main(void)
          if (!io0_hold_low)
             gpio_connectivity_mpu_replay_io0_sample_report();
       if (!io1_hold_high)
-         gpio_connectivity_mpu_replay_io1_sample_report();
+         if (!io1_hold_low)
+            gpio_connectivity_mpu_replay_io1_sample_report();
       if (!io2_hold_high)
          gpio_connectivity_mpu_replay_io2_sample_report();
       if (!io3_hold_high)
